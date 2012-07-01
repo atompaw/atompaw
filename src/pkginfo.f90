1a42aa0300838bc16460b307b6026e10334bbe17
natalie <natalie@3af24c23-0d1c-4fef-b98d-cde60eb54426>
2012-07-01 21:39:04 +0000

    Slight change to pkginfo.f90   (temporary fix)

--- src/pkginfo.f90
+++ src/pkginfo.f90
@@ -5,7 +5,7 @@ MODULE PkgInfo
   PUBLIC
 
   CHARACTER(len=*), PARAMETER :: atp_package = "atompaw"
-  CHARACTER(len=*), PARAMETER :: atp_version = "3.0.1.4"
+  CHARACTER(len=*), PARAMETER :: atp_version = "4.0.0.0"
   CHARACTER(len=*), PARAMETER :: atp_host    = "x86_64-unknown-linux-gnu"
   CHARACTER(len=*), PARAMETER :: atp_target  = "x86_64-unknown-linux-gnu"
 

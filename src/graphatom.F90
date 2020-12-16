5114b842df3449f0c25f2ecc99b537443729c3fd
Natalie Holzwarth <natalie@wfu.edu>
2020-12-15 20:39:57 -0500

    graphatom.F90 minor change

--- src/graphatom.F90
+++ src/graphatom.F90
@@ -57,11 +57,13 @@ PROGRAM graphatom
          CALL SCFatom('NC',lotsofoutput)
          CALL Report_Graphatom('NC',Grid,AEOrbit,AEPot,AESCF)
        ELSE IF (i == 2) THEN
+         STOP 'Frozencore not yet programmed for input_dataset_mod '
          CALL SCFatom('SC',lotsofoutput)
          write(std_out,*) ' Finished SC in graphatom '; call flush_unit(std_out)
          CALL Report_Graphatom('FC',Grid,FCOrbit,FCPot,FCSCF)
          write(std_out,*) ' Finished SC report in graphatom '; call flush_unit(std_out)
        ELSE IF (i == 3) THEN
+         STOP 'Frozencore not yet programmed for input_dataset_mod '
          write(std_out,*) 'before FC in graphtom' ; call flush_unit(std_out)
          CALL SCFatom('FC',lotsofoutput)
          CALL Report_Graphatom('FC',Grid,FCOrbit,FCPot,FCSCF)

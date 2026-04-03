4be2a3c30fbcf9eb8fd9f99782b5acd1f0f194ff
Marc Torrent <marc.torrent@cea.fr>
2026-04-03 17:50:20 +0200

    Adjust library detection in CMake build system

--- cmake/get_libxc.cmake
+++ cmake/get_libxc.cmake
@@ -21,70 +21,85 @@ if (USE_LIBXC)
   find_package(Libxc QUIET)
   if (Libxc_FOUND)
     message(STATUS "Libxc found via cmake target")
-
+    set(LIBXC_FOUND TRUE)
     set(Libxc_FOUND_with_cmake TRUE)
+  endif()
 
 # 2- Try via PKGCONFIG
-  else()
+  if (NOT LIBXC_FOUND)
     if (PkgConfig_FOUND)
       pkg_check_modules(LIBXC QUIET IMPORTED_TARGET libxc)
-    else()
-      set(LIBXC_FOUND FALSE)
     endif()
     if (LIBXC_FOUND)
       message(STATUS "Libxc found via pkg-config: ${LIBXC_VERSION}")
       if (NOT TARGET Libxc::xc)
         add_library(Libxc::xc ALIAS PkgConfig::LIBXC)
       endif()
+      set(Libxc_VERSION ${LIBXC_VERSION})
       get_target_property(Libxc_LIBRARIES Libxc::xc INTERFACE_LINK_LIBRARIES)
       get_target_property(Libxc_INCLUDE_DIRS Libxc::xc INTERFACE_INCLUDE_DIRECTORIES)
-      set(Libxc_VERSION ${LIBXC_VERSION})
-
-      set(Libxc_FOUND_with_pkgconfig TRUE)
+      if (NOT Libxc_INCLUDE_DIRS OR Libxc_INCLUDE_DIRS STREQUAL "Libxc_INCLUDE_DIRS-NOTFOUND")
+        set(Libxc_INCLUDE_DIRS ${LIBXC_INCLUDE_DIRS})
+      endif()
+      if (NOT Libxc_INCLUDE_DIRS)
+        pkg_get_variable(Libxc_INCLUDE_DIRS libxc includedir)
+      endif()
+      if (NOT Libxc_INCLUDE_DIRS)
+        find_path(Libxc_INCLUDE_DIRS NAMES xc.h
+                  HINTS ${LIBXC_PREFIX}/include
+                  ${LIBXC_LIBRARY_DIRS}/../include
+                  PATH_SUFFIXES libxc)
+      endif()
+      if (NOT Libxc_INCLUDE_DIRS)
+        set(LIBXC_FOUND FALSE)
+      else()
+        set(Libxc_FOUND_with_pkgconfig TRUE)
+      endif()
+    endif()
+  endif()
 
 # 3- Try via environment variables
-    else()
-      if (NOT LIBXC_ROOT)
-        set(LIBXC_ROOT "$ENV{LIBXC_ROOT}" CACHE PATH "Libxc root")
-      endif()
-      if (NOT LIBXC_ROOT)
-        set(LIBXC_ROOT "$ENV{LIBXC_DIR}" CACHE PATH "Libxc root")
-      endif()
-      string(REPLACE ":" ";" _ld_paths "$ENV{LD_LIBRARY_PATH}")
-      find_path(LIBXC_INCLUDE_DIR
-                NAMES xc.h xc_funcs.h
-                HINTS
-                  "${LIBXC_ROOT}/include"
-                  "${_ld_paths}/../include"
-                PATH_SUFFIXES "" libxc xc
-                NO_DEFAULT_PATH)
-      find_library(LIBXC_LIBRARY
-                   NAMES xc
-                   HINTS
-                     "${LIBXC_ROOT}/lib"
-                     "${LIBXC_ROOT}/lib64"
-                     ${_ld_paths}
-                   NO_DEFAULT_PATH)
-      if (LIBXC_INCLUDE_DIR AND LIBXC_LIBRARY AND NOT TARGET Libxc::xc)
-        message(STATUS "Libxc found in environment: ${LIBXC_LIBRARY}")
-        add_library(Libxc::xc UNKNOWN IMPORTED)
-        set_target_properties(Libxc::xc PROPERTIES
-            IMPORTED_LOCATION "${LIBXC_LIBRARY}"
-            INTERFACE_INCLUDE_DIRECTORIES "${LIBXC_INCLUDE_DIR}")
-        set(Libxc_INCLUDE_DIRS ${LIBXC_INCLUDE_DIR})
-        set(Libxc_LIBRARIES ${LIBXC_LIBRARY})
+  if (NOT LIBXC_FOUND)
+    if (NOT LIBXC_ROOT)
+      set(LIBXC_ROOT "$ENV{LIBXC_ROOT}" CACHE PATH "Libxc root")
+    endif()
+    if (NOT LIBXC_ROOT)
+      set(LIBXC_ROOT "$ENV{LIBXC_DIR}" CACHE PATH "Libxc root")
+    endif()
+    string(REPLACE ":" ";" _ld_paths "$ENV{LD_LIBRARY_PATH}")
+    find_path(LIBXC_INCLUDE_DIR
+              NAMES xc.h xc_funcs.h
+              HINTS
+                "${LIBXC_ROOT}/include"
+                "${_ld_paths}/../include"
+              PATH_SUFFIXES "" libxc xc
+              NO_DEFAULT_PATH)
+    find_library(LIBXC_LIBRARY
+                 NAMES xc
+                 HINTS
+                   "${LIBXC_ROOT}/lib"
+                   "${LIBXC_ROOT}/lib64"
+                   ${_ld_paths}
+                 NO_DEFAULT_PATH)
+    if (LIBXC_INCLUDE_DIR AND LIBXC_LIBRARY AND NOT TARGET Libxc::xc)
+      message(STATUS "Libxc found in environment: ${LIBXC_LIBRARY}")
+      add_library(Libxc::xc UNKNOWN IMPORTED)
+      set_target_properties(Libxc::xc PROPERTIES
+          IMPORTED_LOCATION "${LIBXC_LIBRARY}"
+          INTERFACE_INCLUDE_DIRECTORIES "${LIBXC_INCLUDE_DIR}")
+      set(Libxc_INCLUDE_DIRS ${LIBXC_INCLUDE_DIR})
+      set(Libxc_LIBRARIES ${LIBXC_LIBRARY})
 
-        # Retrieve Libxc_VERSION
-        file(STRINGS "${LIBXC_INCLUDE_DIR}/xc_version.h" _xc_version_line
-             REGEX "^#define[ \t]+XC_VERSION[ \t]+\"[0-9.]+\"")
-        string(REGEX REPLACE ".*XC_VERSION[ \t]+\"([0-9.]+)\".*" "\\1"
-               Libxc_VERSION "${_xc_version_line}")
-        if (NOT Libxc_VERSION)
-          set(Libxc_VERSION "undefined")
-        endif()
-
-        set(Libxc_FOUND_with_environment TRUE)
+      # Retrieve Libxc_VERSION
+      file(STRINGS "${LIBXC_INCLUDE_DIR}/xc_version.h" _xc_version_line
+           REGEX "^#define[ \t]+XC_VERSION[ \t]+\"[0-9.]+\"")
+      string(REGEX REPLACE ".*XC_VERSION[ \t]+\"([0-9.]+)\".*" "\\1"
+             Libxc_VERSION "${_xc_version_line}")
+      if (NOT Libxc_VERSION)
+        set(Libxc_VERSION "undefined")
       endif()
+
+      set(Libxc_FOUND_with_environment TRUE)
     endif()
   endif()
 
--- cmake/get_linalg.cmake
+++ cmake/get_linalg.cmake
@@ -17,13 +17,12 @@ find_package(BLAS QUIET)
 if (BLAS_FOUND)
   message(STATUS "BLAS found via cmake target")
   set(BLAS_FOUND_with_cmake TRUE)
+endif()
 
 # 2- Try BLAS via PKGCONFIG
-else()
+if (NOT BLAS_FOUND)
   if (PkgConfig_FOUND)
-#    pkg_check_modules(BLAS QUIET IMPORTED_TARGET blas openblas)
-  else()
-    set(BLAS_FOUND FALSE)
+    pkg_check_modules(BLAS QUIET IMPORTED_TARGET blas openblas)
   endif()
   if (BLAS_FOUND)
     message(STATUS "BLAS found via pkg-config")
@@ -31,31 +30,35 @@ else()
       add_library(BLAS::BLAS ALIAS PkgConfig::BLAS)
     endif()
     get_target_property(BLAS_LIBRARIES BLAS::BLAS INTERFACE_LINK_LIBRARIES)
-    set(BLAS_FOUND_with_pkgconfig TRUE)
+    if (NOT BLAS_LIBRARIES)
+      set(BLAS_FOUND FALSE)
+    else()
+      set(BLAS_FOUND_with_pkgconfig TRUE)
+    endif()
+  endif()
+endif()
 
 # 3- Try BLAS via environment variables
-  else()
-    if (NOT BLAS_ROOT)
-      set(BLAS_ROOT "$ENV{BLAS_ROOT}" CACHE PATH "BLAS root")
-    endif()
-    if (NOT BLAS_ROOT)
-      set(BLAS_ROOT "$ENV{BLAS_DIR}" CACHE PATH "BLAS root")
-    endif()
-    find_library(BLAS_LIBRARY
-                 NAMES blas openblas
-                 HINTS
-                   "${BLAS_ROOT}/lib"
-                   "${BLAS_ROOT}/lib64"
-                   "${_ld_paths}"
-                 NO_DEFAULT_PATH)
-    if (BLAS_LIBRARY AND NOT TARGET BLAS::BLAS)
-      message(STATUS "BLAS found in environment: ${BLAS_LIBRARY}")
-      add_library(BLAS::BLAS UNKNOWN IMPORTED)
-      set_target_properties(BLAS::BLAS PROPERTIES
-          IMPORTED_LOCATION "${BLAS_LIBRARY}")
-      set(BLAS_LIBRARIES ${BLAS_LIBRARY})
-      set(BLAS_FOUND_with_environment TRUE)
-    endif()
+if (NOT BLAS_FOUND)
+  if (NOT BLAS_ROOT)
+    set(BLAS_ROOT "$ENV{BLAS_ROOT}" CACHE PATH "BLAS root")
+  endif()
+  if (NOT BLAS_ROOT)
+    set(BLAS_ROOT "$ENV{BLAS_DIR}" CACHE PATH "BLAS root")
+  endif()
+  find_library(BLAS_LIBRARY
+               NAMES blas openblas
+               HINTS
+                 "${BLAS_ROOT}/lib"
+                 "${BLAS_ROOT}/lib64"
+                 "${_ld_paths}"
+               NO_DEFAULT_PATH)
+  if (BLAS_LIBRARY AND NOT TARGET BLAS::BLAS)
+    message(STATUS "BLAS found in environment: ${BLAS_LIBRARY}")
+    add_library(BLAS::BLAS UNKNOWN IMPORTED)
+    set_target_properties(BLAS::BLAS PROPERTIES IMPORTED_LOCATION "${BLAS_LIBRARY}")
+    set(BLAS_LIBRARIES ${BLAS_LIBRARY})
+    set(BLAS_FOUND_with_environment TRUE)
   endif()
 endif()
 
@@ -66,9 +69,10 @@ find_package(LAPACK QUIET)
 if (LAPACK_FOUND)
   message(STATUS "LAPACK found via cmake target")
   set(LAPACK_FOUND_with_cmake TRUE)
+endif()
 
 # 2- Try LAPACK via PKGCONFIG
-else()
+if (NOT LAPACK_FOUND)
   if (PkgConfig_FOUND)
     pkg_check_modules(LAPACK QUIET IMPORTED_TARGET lapack openblas)
   else()
@@ -80,31 +84,35 @@ else()
       add_library(LAPACK::LAPACK ALIAS PkgConfig::LAPACK)
     endif()
     get_target_property(LAPACK_LIBRARIES Libxc::xc INTERFACE_LINK_LIBRARIES)
-    set(LAPACK_FOUND_with_pkgconfig TRUE)
+    if (NOT LAPACK_LIBRARIES)
+      set(LAPACK_FOUND FALSE)
+    else()
+      set(LAPACK_FOUND_with_pkgconfig TRUE)
+    endif()
+  endif()
+endif()
 
 # 3- Try LAPACK via environment variables
-  else()
-    if (NOT LAPACK_ROOT)
-      set(LAPACK_ROOT "$ENV{LAPACK_ROOT}" CACHE PATH "LAPACK root")
-    endif()
-    if (NOT LAPACK_ROOT)
-      set(LAPACK_ROOT "$ENV{LAPACK_DIR}" CACHE PATH "LAPACK root")
-    endif()
-    find_library(LAPACK_LIBRARY
-                 NAMES lapack openblas
-                 HINTS
-                   "${LAPACK_ROOT}/lib"
-                   "${LAPACK_ROOT}/lib64"
-                   "${_ld_paths}"
-                 NO_DEFAULT_PATH)
-    if (LAPACK_LIBRARY AND NOT TARGET LAPACK::LAPACK)
-      message(STATUS "LAPACK found in environment: ${LAPACK_LIBRARY}")
-      add_library(LAPACK::LAPACK UNKNOWN IMPORTED)
-      set_target_properties(LAPACK::LAPACK PROPERTIES
-          IMPORTED_LOCATION "${LAPACK_LIBRARY}")
-      set(LAPACK_LIBRARIES ${LAPACK_LIBRARY})
-      set(LAPACK_FOUND_with_environment TRUE)
-    endif()
+if (NOT LAPACK_FOUND)
+  if (NOT LAPACK_ROOT)
+    set(LAPACK_ROOT "$ENV{LAPACK_ROOT}" CACHE PATH "LAPACK root")
+  endif()
+  if (NOT LAPACK_ROOT)
+    set(LAPACK_ROOT "$ENV{LAPACK_DIR}" CACHE PATH "LAPACK root")
+  endif()
+  find_library(LAPACK_LIBRARY
+               NAMES lapack openblas
+               HINTS
+                 "${LAPACK_ROOT}/lib"
+                 "${LAPACK_ROOT}/lib64"
+                 "${_ld_paths}"
+               NO_DEFAULT_PATH)
+  if (LAPACK_LIBRARY AND NOT TARGET LAPACK::LAPACK)
+    message(STATUS "LAPACK found in environment: ${LAPACK_LIBRARY}")
+    add_library(LAPACK::LAPACK UNKNOWN IMPORTED)
+    set_target_properties(LAPACK::LAPACK PROPERTIES IMPORTED_LOCATION "${LAPACK_LIBRARY}")
+    set(LAPACK_LIBRARIES ${LAPACK_LIBRARY})
+    set(LAPACK_FOUND_with_environment TRUE)
   endif()
 endif()
 
